e40c6dfe5e5f56f6dd0d3666581d17ed070de700
Ivan Gurkov <ivangurkoff@gmail.com>
2025-12-12 16:09:54 +0300

    colorscheming and stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- vim.cmd("color christmas")
 vim.cmd("color catppuccin")
 vim.cmd("highlight Normal guibg=NONE")
 vim.cmd("highlight NormalNC guibg=NONE")
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,22 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    auto_integrations = true,
-    flavour = "mocha",
-    transparent_background = true,
-    integrations = {
-      neotree = true,
-      -- bufferline = true,
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      auto_integrations = true,
+      flavour = "mocha",
+      transparent_background = true,
+      integrations = {
+        neotree = true,
+        -- bufferline = true,
+      },
     },
   },
+  -- {
+  --   "chaserensberger/christmas.nvim",
+  --   name = "christmas",
+  --   lazy = false,
+  -- },
 }
