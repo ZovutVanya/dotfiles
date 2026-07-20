f2e2705f50b01858d14e4ef0a0697b6cdb9f2851
Ivan Gurkov <ivangurkoff@gmail.com>
2026-07-20 11:19:00 +0300

    nvim-puppeteer on python only

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -73,6 +73,7 @@ return {
   },
   {
     "chrisgrieser/nvim-puppeteer",
-    lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+    -- lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
+    ft = "python",
   },
 }
